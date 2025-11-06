65cefb5c4b6db12c882e63a11688798e820a46c0
erfan tolou <erfantolou@gmail.com>
2025-11-06 19:13:39 +0330

    feat: add About, Blog, Projects, Services, and Contact pages with content and structure

---
+++ src/app/[locale]/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to our application! We are dedicated to providing the best service possible.</p>
+            <p>Our mission is to deliver high-quality products that meet the needs of our customers.</p>
+            <p>Thank you for visiting our About page!</p>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/[locale]/blog/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BlogPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <article>
+                <h2>First Blog Post</h2>
+                <p>This is the content of the first blog post.</p>
+            </article>
+            <article>
+                <h2>Second Blog Post</h2>
+                <p>This is the content of the second blog post.</p>
+            </article>
+        </div>
+    );
+};
+
+export default BlogPage;
